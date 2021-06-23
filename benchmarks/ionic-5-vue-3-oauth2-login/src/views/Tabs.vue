9d587c95d8e4449affe5369080557a6c3068354d
bottlehs <qudgns9@gmail.com>
2021-06-23 16:06:50 +0900

    ionic-5-vue-3-oauth2-login

--- benchmarks/ionic-5-vue-3-oauth2-login/src/views/Tabs.vue
+++ benchmarks/ionic-5-vue-3-oauth2-login/src/views/Tabs.vue
@@ -1,7 +1,6 @@
 <template>
   <ion-page>
     <ion-tabs>
-      <ion-router-outlet />
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
           <ion-icon :icon="triangle" />
