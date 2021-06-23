3716936895a5926ddefcd7644f9e6b35b752f85a
bottlehs <qudgns9@gmail.com>
2021-06-23 18:04:15 +0900

    composition-api 예시

--- benchmarks/vuecompositionapi/src/main.js
+++ benchmarks/vuecompositionapi/src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import VueCompositionApi from '@vue/composition-api' 
+Vue.use(VueCompositionApi)
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
